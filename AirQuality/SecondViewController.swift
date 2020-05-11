7b8cdb1df704fde1270698da857e3d48adf830c2
Adam Talarczyk <Adam.Talarczyk@hotmail.com>
2020-05-11 16:45:25 +0200

    Initial Commit

---
+++ AirQuality/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AirQuality
+//
+//  Created by Talar on 11/05/2020.
+//  Copyright © 2020 Talarczyk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
