c199203230c779372adc7f5e0fc92dd0f599c9f3
Adam Talarczyk <Adam.Talarczyk@hotmail.com>
2020-05-22 11:03:55 +0200

    View update

--- AirQuality/DetailsViewController.swift
+++ AirQuality/DetailsViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var PMOutlet: UIView!
+    @IBOutlet weak var PM10Outlet: UIView!
+    @IBOutlet weak var SO2Outlet: UIView!
+    @IBOutlet weak var NO2Outlet: UIView!
+    @IBOutlet weak var COOutlet: UIView!
+    @IBOutlet weak var C6H6Outlet: UIView!
+    @IBOutlet weak var O3Outlet: UIView!
+    
+    @IBOutlet weak var IndexNameLabel: UILabel!
+    @IBOutlet weak var StationNameLabel: UILabel!
+    @IBOutlet weak var PM25Label: UILabel!
+    @IBOutlet weak var PM10Label: UILabel!
+    @IBOutlet weak var SO2Label: UILabel!
+    @IBOutlet weak var NO2Label: UILabel!
+    @IBOutlet weak var COLabel: UILabel!
+    @IBOutlet weak var C6H6Label: UILabel!
+    @IBOutlet weak var O3Label: UILabel!
+    
     @IBAction func BookmarkAction(_ sender: Any) {
         print("bookmark performed")
     }
     
-    @IBOutlet weak var IndexNameLabel: UILabel!
-    
-    @IBOutlet weak var StationNameLabel: UILabel!
     public var station: Station?
     public var indexLevel: IndexLevel?
     var sensorList = [Sensor]()
@@ -38,6 +53,16 @@ class DetailsViewController: UIViewController {
     func afterViewLoad(){
         StationNameLabel.text = self.station?.stationName
         IndexNameLabel.text = self.indexLevel?.stIndexLevel?.indexLevelName
+        
+        self.formatOutlet(elements: [PMOutlet, PM10Outlet, SO2Outlet, NO2Outlet, COOutlet, C6H6Outlet, O3Outlet])
+    }
+    
+    func formatOutlet(elements: [UIView]){
+        for element in elements{
+            element.layer.borderWidth = 1
+            element.layer.borderColor = UIColor.systemBlue.cgColor
+            element.layer.cornerRadius = 8
+        }
     }
     
     func getIndexLevel(){
@@ -69,7 +94,9 @@ class DetailsViewController: UIViewController {
             let sensorValueController = IdentifiedDataFetchController(endpoint: EndpointList.sensorValue, idObject: sensor.id)
             sensorValueController.fetchAllData{ (data, response, err) in
                 self.sensorValueList = dataConverter.prepareData(data: data)
+                print(self.sensorValueList)
             }
+            
         }
     }
     
